b3747850efb8a4e38658fc5e155804c4246bf8ac
Shane Chen <zshanechen@gmail.com>
2025-02-22 16:29:30 -0500

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -19,7 +20,7 @@ export default function RootLayout({
           Shane Chen&apos;s Website
         </title>
       </head>
-      <body>{children}</body>
+      <body>{children}<Analytics /></body>
     </html>
   );
 }
