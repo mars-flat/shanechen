eb4e79159e144972b896f8b52712c8339de18515
Shane Chen <zshanechen@gmail.com>
2025-09-18 18:06:05 -0400

    updates to resume

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import ParallaxClientProvider from "./components/ParallaxClientProvider";
 
 export const metadata: Metadata = {
-  title: "Shane Chen's Website",
+  title: "Shane Chen",
   icons: "/images/favicon.png",
   description: "I'm a computer science student at the University of Waterloo, class of 2029. I'm currently looking for summer 2025 internships. Let's have a chat!",
 };
